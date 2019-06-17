ef19dec3dc872ba443600c9f9a2bd13d2e392771
marzomar <omar@frenchcoincoin.com>
2019-06-17 17:39:54 +0200

    Template tailwind nunjunck

---
+++ nunjucks.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    root: "./src/html",
+    data: {
+        name: "omar",
+        subtitle:"",
+        services: [
+            {
+                title: "Commander un acte d'état-civil",
+                categories: "Download"
+            },
+            {
+                title: "Camping municipal",
+                categories: "Book"
+            },
+            {
+                title: "Taxis : autorisation de stationnement",
+                categories: "Book"
+            },
+            {
+                title: "Location d'une parcelle de jardin",
+                categories: "Ask"
+            },
+            {
+                title: "Salles municipales",
+                categories: "Book"
+            },
+            {
+                title: "Boucles d'amarrage",
+                categories: "Ask"
+            },
+            {
+                title: "Marchés, foire, fête",
+                categories: "Ask"
+            },
+            {
+                title: "Meublés de tourisme, chambres d'hôte",
+                categories: "Ask"
+            },
+        ],
+        articles: [
+            {
+                date: "27 OCT. 28 OCT.",
+                title: "Titre1",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "28 OCT.",
+                title: "Titre2",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "02 NOV.",
+                title: "Titre3",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "10 NOV.",
+                title: "Titre4",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            }
+        ]
+ },
+}
